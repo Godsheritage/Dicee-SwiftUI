0814c9812c96d21ba6500f0c31fe7c512e0d7352
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-02-17 15:15:57 -0500

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Godsheritage Adeoye on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
