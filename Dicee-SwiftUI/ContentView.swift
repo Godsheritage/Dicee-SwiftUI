027aa680cc20e9780c900fc5c55f103733fc87f2
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-02-19 20:48:46 -0500

    updates to the Dice App

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -7,20 +7,66 @@
 
 import SwiftUI
 
-struct ContentView: View {
+ struct ContentView: View {
+    
+    
+    
+    @State var rightDiceNumber = 1
+    @State var leftDiceNumber = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+       
+        ZStack{
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: rightDiceNumber)
+                    DiceView(n: leftDiceNumber)
+                    
+                }
+                Spacer()
+                 Button("Roll") {
+                     self.leftDiceNumber = Int.random(in: 1...6)
+                     self.rightDiceNumber = Int.random(in: 1...6)
+                }
+                .font(.system(size: 50))
+                .fontWeight(.heavy)
+                .foregroundColor(Color.white)
+                .background(Color.red)
+                .padding()
+            }
         }
-        .padding()
+        
+        
+
+        
+        
+        
     }
 }
+
+struct DiceView: View {
+    var n : Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .frame(height: 130)
+            .padding(.all)
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
